c5fa83435fc6944268aed383186e0a4bcb523b46
EstevenMart <emartine0829@gmail.com>
2021-07-23 16:41:50 -0500

    Conexion y crud

--- ProyectoARL/src/java/Util/Crud.java
+++ ProyectoARL/src/java/Util/Crud.java
@@ -10,5 +10,12 @@ package Util;
  * @author emart
  */
 public interface Crud {
+
+    public abstract boolean agregarRegistro();
+
+    public abstract boolean actualizarRegistro();
+
+    public abstract boolean eliminarRegistro();
+
     
 }
